c5b4d56b0e101341b906e7151d4dcd9815af3ab2
brianbigdelle <brianbigdelle@gmail.com>
2024-02-11 10:26:38 -0800

    changed image route for build

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <img src='../public/selknam_tiny.jpg'></img>
+    <img src='../selknam_tiny.jpg'></img>
     <h2>1Yarn is the future of writing contests.</h2>
     <h1>Join our Mailing List</h1>
     <App />
